47bd4cbe643679162fa696b06341299d85a99cbb
Maciej Sroka <sroka.maciej93@gmail.com>
2022-07-23 20:17:52 +0200

    Remove dropdown menu if not needed and add it if needed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
-import initDropdowns from '@hysky93/dropdown-menu';
+import Menu from './modules/menu';
 import './style.css';
-import menu from './modules/menu';
 
+const navbar = document.querySelector('.navbar');
+const links = [];
+
+for (let i = 0; i < 12; i++) {
+  const navItem = document.createElement('li');
+  const navLink = document.createElement('a');
+  navLink.textContent = `Link ${i}`;
+  navItem.appendChild(navLink);
+
+  links.push(navItem);
+}
+
+const menu = Menu(navbar, links);
+menu.createMenu();
+menu.moveLinks();
 window.onresize = menu.moveLinks;
-initDropdowns();
--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,15 +1,16 @@
-const menu = (() => {
+const Menu = (parent, [...links]) => {
   const isInViewport = (element) => {
     const dropdown = document.querySelector('.dropdown');
     const rect = element.getBoundingClientRect();
-    const rectDropdown = dropdown.getBoundingClientRect();
-    return (
-      rect.top >= 0 &&
+    const rectDropdown = dropdown ? dropdown.getBoundingClientRect() : null;
+    return rect.top >= 0 &&
       rect.left >= 0 &&
       rect.bottom <=
         (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= rectDropdown.left
-    );
+      dropdown
+      ? rect.right <= rectDropdown.left
+      : rect.right <=
+          (window.innerWidth || document.documentElement.clientWidth);
   };
 
   const isFreeSpace = (link) => {
@@ -20,28 +21,70 @@ const menu = (() => {
     const freeSpace = rect.right - rectDropdown.right;
     const linkWidth = link.clientWidth;
 
-    return freeSpace >= linkWidth;
+    return freeSpace >= linkWidth + 24;
+  };
+
+  const appendLinks = (menu) => {
+    links.forEach((link) => {
+      menu.appendChild(link);
+    });
+  };
+
+  const createDropdown = () => {
+    const dropdown = document.createElement('div');
+    const collapsable = document.createElement('div');
+    const title = document.createElement('span');
+    title.classList.add('.dropdown-title');
+    dropdown.classList.add('dropdown');
+    collapsable.classList.add('dropdown-collapse');
+    title.textContent = 'More ↓';
+    dropdown.append(title, collapsable);
+
+    parent.appendChild(dropdown);
+  };
+
+  const createMenu = () => {
+    const menu = document.createElement('ul');
+    menu.classList.add('menu--progressive');
+    parent.appendChild(menu);
+    appendLinks(menu);
+    createDropdown();
   };
 
   const moveLinks = () => {
     const menuContainer = document.querySelector('.menu--progressive');
-    const dropdown = document.querySelector('.dropdown-menu');
+    const dropdown = document.querySelector('.dropdown-collapse');
     const menulinks = [...menuContainer.children];
 
     menulinks.forEach((link) => {
       if (!isInViewport(link)) {
-        menuContainer.removeChild(link);
-        dropdown.appendChild(link);
+        if (!dropdown) {
+          createDropdown();
+          const newDropdown = document.querySelector('.dropdown-collapse');
+          menuContainer.removeChild(link);
+          newDropdown.appendChild(link);
+        } else {
+          menuContainer.removeChild(link);
+          dropdown.appendChild(link);
+        }
       }
     });
+    const link = dropdown ? dropdown.lastElementChild : null;
 
-    const link = dropdown.lastChild;
-    if (isFreeSpace(link)) {
-      dropdown.removeChild(link);
-      menuContainer.appendChild(link);
+    if (link) {
+      if (isFreeSpace(link)) {
+        dropdown.removeChild(link);
+        menuContainer.appendChild(link);
+      }
+    }
+
+    if (dropdown) {
+      if (!dropdown.lastElementChild) {
+        parent.removeChild(dropdown.parentElement);
+      }
     }
   };
-  return { moveLinks };
-})();
+  return { createMenu, moveLinks };
+};
 
-export default menu;
+export default Menu;
