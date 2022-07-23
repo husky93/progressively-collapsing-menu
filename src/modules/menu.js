2326b8e4863f9bf9c4adb69e17ced718d6e9afd4
Maciej Sroka <sroka.maciej93@gmail.com>
2022-07-23 19:03:07 +0200

    Fix function that checks if links are overflowing

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,15 +1,30 @@
 const menu = (() => {
   const isInViewport = (element) => {
+    const dropdown = document.querySelector('.dropdown');
     const rect = element.getBoundingClientRect();
+    const rectDropdown = dropdown.getBoundingClientRect();
     return (
       rect.top >= 0 &&
       rect.left >= 0 &&
       rect.bottom <=
         (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+      rect.right <= rectDropdown.left
     );
   };
-  return { value };
+
+  const moveOverflowingLinks = () => {
+    const menuContainer = document.querySelector('.menu--progressive');
+    const dropdown = document.querySelector('.dropdown');
+    const links = [...menuContainer.children];
+
+    links.forEach((link) => {
+      if (!isInViewport(link)) {
+        console.log('yo');
+      }
+    });
+    console.log(links);
+  };
+  return { moveOverflowingLinks };
 })();
 
 export default menu;
