e3b061d1462bbd023fb45614c98bf7b96c042c06
Maciej Sroka <sroka.maciej93@gmail.com>
2022-07-24 18:16:42 +0200

    Update readme

--- README.md
+++ README.md
@@ -1,1 +1,66 @@
 # progressively-collapsing-menu
+
+Progressively collapsing menu component. Moves links that are overflowing viewport inside a dropdown menu. Highly customisable as there is only basic styling applied.
+
+## How to use
+
+##### - Install the module inside your project
+
+```
+npm install progressively-collapsing-menu --save
+```
+
+##### - Import it with your preferred method and call the imported function
+
+```javascript
+import Menu from 'progressively-collapsing-menu';
+const menu = Menu(parent, links, {
+  dropdownText: 'Show more'
+});
+```
+
+###### Function takes 3 parameters:
+- `parent` - a DOM element, container in which your menu will be nested.
+- `links` - an array of DOM elements containing all your menu links.
+- `options` - an object with optional additional parameters. Those parameters are:
+  - `dropdownText` - Changes the default text of the dropdown menu. Must be a string.
+  - `noStyles` - Removes all the basic styling. :warning: **Use with caution**: This option may cause issues. It breaks the custom dropdown animation and may also break whole menu component.
+
+###### It returns an object with two methods:
+- `createMenu` - Creates and renders menu with provided `links` inside the specified `parent` container.
+- `moveLinks` - Checks if links are overflowing or if there is free space and moves them between menu and dropdown accordingly.
+
+##### - Create menu
+
+Create menu and call `moveLinks` method whenever you want to check if links are overflowing and move them inside the dropdown, or move them from dropdown to menu if there is free space available.
+
+```javascript
+menu.createMenu();
+menu.moveLinks();
+window.onresize = menu.moveLinks;
+```
+
+### Full use example with webpack
+
+```javascript
+import Menu from 'progressively-collapsing-menu';
+
+const container = document.querySelector('.container');
+const links = [];
+
+for (let i = 0; i < 24; i++) {
+  const navItem = document.createElement('li');
+  const navLink = document.createElement('a');
+  navLink.textContent = `Link ${i}`;
+  navItem.appendChild(navLink);
+  links.push(navItem);
+}
+
+const menu = Menu(container, links, {
+  dropdownText: 'Show more'
+});
+
+menu.createMenu();
+menu.moveLinks();
+window.onresize = menu.moveLinks;
+```
--- src/index.js
+++ src/index.js
@@ -1,17 +1,129 @@
-import Menu from './modules/menu';
+const Menu = (parent, [...links], options = {}) => {
+  const isInViewport = (element) => {
+    const dropdown = document.querySelector('.dropdown');
+    const rect = element.getBoundingClientRect();
+    const rectDropdown = dropdown ? dropdown.getBoundingClientRect() : null;
+    return dropdown
+      ? rect.right <= rectDropdown.left
+      : rect.right <=
+          (window.innerWidth || document.documentElement.clientWidth);
+  };
 
-const navbar = document.querySelector('.menu');
-const links = [];
+  const isFreeSpace = () => {
+    const navbar = document.querySelector('.menu-wrapper');
+    const dropdown = document.querySelector('.dropdown');
+    const dropdownList = document.querySelector('.dropdown-collapse');
+    const rect = navbar.getBoundingClientRect();
+    const rectDropdown = dropdown.getBoundingClientRect();
+    const freeSpace = rect.right - rectDropdown.right;
+    const linkWidth = dropdownList.lastElementChild.clientWidth;
 
-for (let i = 0; i < 35; i++) {
-  const navItem = document.createElement('li');
-  const navLink = document.createElement('a');
-  navLink.textContent = `Link ${i}`;
-  navItem.appendChild(navLink);
-  links.push(navItem);
-}
+    return freeSpace >= linkWidth;
+  };
 
-const menu = Menu(navbar, links, {});
-menu.createMenu();
-menu.moveLinks();
-window.onresize = menu.moveLinks;
+  const appendLinks = (menu) => {
+    links.forEach((link) => {
+      menu.appendChild(link);
+    });
+  };
+
+  const createDropdown = (wrapper) => {
+    const dropdown = document.createElement('div');
+    const collapsable = document.createElement('div');
+    const title = document.createElement('span');
+
+    title.classList.add('dropdown-title');
+    dropdown.classList.add('dropdown');
+    collapsable.classList.add('dropdown-collapse');
+
+    title.textContent =
+      typeof options.dropdownText === 'string'
+        ? options.dropdownText
+        : 'More ↓';
+
+    dropdown.addEventListener('click', () =>
+      dropdown.classList.toggle('active')
+    );
+
+    dropdown.append(title, collapsable);
+    wrapper.appendChild(dropdown);
+  };
+
+  const addDropdownStyles = () => {
+    require('./assets/dropdown.css');
+  };
+
+  const addMenuStyles = () => {
+    require('./assets/menu.css');
+  };
+
+  const createMenu = () => {
+    const menu = document.createElement('ul');
+    const wrapper = document.createElement('nav');
+
+    wrapper.classList.add('menu-wrapper');
+    menu.classList.add('menu--progressive');
+
+    wrapper.style.display = 'flex';
+    wrapper.style.flexDirection = 'row';
+    menu.style.display = 'flex';
+    menu.style.flexDirection = 'row';
+    menu.style.overflow = 'hidden';
+    menu.style.listStyle = 'none';
+
+    wrapper.appendChild(menu);
+    parent.appendChild(wrapper);
+
+    appendLinks(menu);
+    createDropdown(wrapper);
+
+    if (!options.noStyles) {
+      addDropdownStyles();
+      addMenuStyles();
+    }
+  };
+
+  const isElementEmpty = (element) => element.lastElementChild === null;
+
+  const moveLinks = () => {
+    const menuContainer = document.querySelector('.menu--progressive');
+    const menulinks = [...menuContainer.children];
+
+    menulinks
+      .slice()
+      .reverse()
+      .forEach((link) => {
+        let dropdown = document.querySelector('.dropdown-collapse');
+        if (!isInViewport(link) && !dropdown) {
+          const wrapper = document.querySelector('.menu-wrapper');
+          createDropdown(wrapper);
+          dropdown = document.querySelector('.dropdown-collapse');
+          menuContainer.removeChild(link);
+          dropdown.appendChild(link);
+        } else if (!isInViewport(link) && dropdown) {
+          menuContainer.removeChild(link);
+          dropdown.appendChild(link);
+        }
+      });
+
+    const dropdown = document.querySelector('.dropdown-collapse');
+    if (dropdown && !isElementEmpty(dropdown)) {
+      const dropdownLinks = [...dropdown.children];
+      dropdownLinks
+        .slice()
+        .reverse()
+        .forEach((link) => {
+          if (isFreeSpace()) {
+            dropdown.removeChild(link);
+            menuContainer.appendChild(link);
+          }
+        });
+    } else if (dropdown && isElementEmpty(dropdown)) {
+      const wrapper = document.querySelector('.menu-wrapper');
+      wrapper.removeChild(dropdown.parentElement);
+    }
+  };
+  return { createMenu, moveLinks };
+};
+
+export default Menu;
